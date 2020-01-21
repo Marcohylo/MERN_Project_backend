5c6f44053afd4926824ecc3bb3023a0c15892a97
Marco Lo <marco1993@live.com>
2020-01-21 16:34:48 +1100

    23
    22332

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// require('dotenv').config();
+
 const express = require("express");
 const bodyParser = require('body-parser');
 const nodemailer = require("nodemailer");
@@ -9,20 +11,50 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.post('/api/form',(req, res) => {
-  console.log(req.body)
+  console.log(req.body.name)
+  nodemailer.createTestAccount((err, account) => {
+    const htmlEmail = `
+      <h3>Contact Details</h3>
+      <ul>
+        <li> Name: ${req.body.name}</li>
+        <li> Email: ${req.body.email}</li>
+      </ul>
+      <h3>Message</h3>
+      <p>${req.body.message}</p>
+    `
+    let transporter = nodemailer.createTransport({
+      service: 'smtp.ethereal.email',
+      auth: {
+        user: 'kale.boyer51@ethereal.email',
+        pass: 'wfcKKB7hXUUygFjnbT',
+      }
+    })
+    let mailOptions = {
+      from: 'xmedicinepower@gmail.com',
+      to: 'marco1993@live.com',
+      replyTo: 'test@testaccount.com',
+      subject: 'New Message',
+      text: req.body.message,
+      html: htmlEmail
+    }
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.log('Error Occured');
+      } else {
+        console.log('Email En route!');
+      }
+
+    // console.log("Message sent: %s", info.messageId);
+    // console.log('Message URL: %s', nodemailer.getTestMessageUrl(info))
+    })
+  })
 })
-
-const PORT = process.env.PORT || 3001
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`)
-});
 
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
-const app = express();
+// const app = express();
 
 // Mongoose
 
