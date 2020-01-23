887ffeda6b6f5cc20fce1c2156b968c8c90b09a1
Marco Lo <marco1993@live.com>
2020-01-23 12:11:34 +1100

    intc
    d

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const router = express.Router();
 const sendMail = require("../controllers/nodeMailer");
 
+app.get('/', function (req, res) {
+    connection.query('select * from user', function (error, results, fields) {
+    if (error) throw error;
+    res.end(JSON.stringify(results));
+  });
+ });
 router.use("/listings", require("./listing-routes"));
 
 router.use("/orders", require("./order-routes"));
